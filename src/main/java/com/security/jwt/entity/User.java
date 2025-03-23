4014c4d3176aec9c7eff1d55f7153e46dead1286
Naresh Kanigiri <kanigirinaresh74@gmail.com>
2025-03-23 18:30:51 +0530

    _added_security

--- src/main/java/com/security/jwt/entity/User.java
+++ src/main/java/com/security/jwt/entity/User.java
@@ -1,11 +1,9 @@
 package com.security.jwt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="User_Table")
 public class User {
 
     @Id
