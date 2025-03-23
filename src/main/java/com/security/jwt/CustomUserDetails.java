1cde4cf57615c96305511a8a3de0aea6b05e9af7
Naresh Kanigiri <kanigirinaresh74@gmail.com>
2025-03-23 19:10:04 +0530

    _added_blue_green

--- src/main/java/com/security/jwt/CustomUserDetails.java
+++ src/main/java/com/security/jwt/CustomUserDetails.java
@@ -4,10 +4,11 @@ import com.security.jwt.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Objects;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+
 
 public class CustomUserDetails implements UserDetails {
 
