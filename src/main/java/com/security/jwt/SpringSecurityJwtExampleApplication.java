a561e4f5f06bcc4a59dae887863f481dfdea671f
Naresh Kanigiri <kanigirinaresh74@gmail.com>
2025-05-02 11:51:16 +0530

    practice_added_runner_in main method
    Signed-off-by: Naresh Kanigiri <kanigirinaresh74@gmail.com>

--- src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
+++ src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
@@ -1,13 +1,18 @@
 package com.security.jwt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityJwtExampleApplication {
+public class SpringSecurityJwtExampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtExampleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
