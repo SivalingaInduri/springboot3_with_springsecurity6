42085484c629c65c20e1321214c7bcdaef6d989c
Naresh Kanigiri <kanigirinaresh74@gmail.com>
2025-05-02 12:04:36 +0530

    Revert "practice_added_runner_in main method"
    This reverts commit a561e4f5
    
    Signed-off-by: Naresh Kanigiri <kanigirinaresh74@gmail.com>

--- src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
+++ src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
@@ -1,6 +1,5 @@
 package com.security.jwt;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class SpringSecurityJwtExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtExampleApplication.class, args);
-		System.out.println("..");
 	}
 
 }
