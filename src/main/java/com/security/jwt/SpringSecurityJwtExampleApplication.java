c4bc2359637792e7a2b4dbd93604eb8e3efa9bf9
Naresh Kanigiri <kanigirinaresh74@gmail.com>
2025-05-02 12:01:31 +0530

    added_sout_main method
    Signed-off-by: Naresh Kanigiri <kanigirinaresh74@gmail.com>

--- src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
+++ src/main/java/com/security/jwt/SpringSecurityJwtExampleApplication.java
@@ -1,6 +1,6 @@
 package com.security.jwt;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
